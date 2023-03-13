fb5b5ab1cfd466b52a4c05d686f1247c70044f33
Kogot490 <51406121+Kogot490@users.noreply.github.com>
2023-03-13 14:02:17 -0600

    1

--- RPS1.js
+++ RPS1.js
@@ -1,1 +1,26 @@
 
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    if (num = 0) {
+        return 'Rock';
+    } else if (num = 1) {
+        return 'Paper';
+    } else if (num = 2) {
+        return 'Scissors';
+    }
+}
+    
+getComputerChoice();
+console.log(getComputerChoice);
+
+
+
+
+
+
+
+
+
+
+
+
